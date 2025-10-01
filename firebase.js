c829a744f9251631fad52352fe9077b8ca7e5bb5
sfknvcghgd <163533237+sfknvcghgd@users.noreply.github.com>
2025-10-01 16:00:04 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
